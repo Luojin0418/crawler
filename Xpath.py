4ffa20bd94caeed3105cd30a5f74be7e513d16e9
Luojin0418 <49770226+Luojin0418@users.noreply.github.com>
2019-12-13 21:27:40 +0800

    可以爬取整本小说

--- Xpath.py
+++ Xpath.py
@@ -2,6 +2,7 @@
 import requests
 import re
 from lxml import etree
+import time
 
 def getPage(url):
     headers={
@@ -19,27 +20,37 @@ def write_to_file(content):
     with open('spider.txt', 'a', encoding='utf-8') as f:
         f.write(content+'\n')
 
+def write_to_href(href):
+    with open('href.txt', 'a', encoding='utf-8') as f:
+        f.write(href+'\n')
 
 # 读取首页并爬取所有的链接
 url0 = "https://www.wenku8.net/novel/1/1973/"
 html0 = etree.HTML(getPage(url0))
 
-herf = html0.xpath('//td/a/@href')
-title0 = html0.xpath('//td[@class="ccss"]/a/text()')
-print(herf)
-print(title0)
+href = html0.xpath('//td/a/@href')
 
-# 爬取某一页上的内容
-url = "https://www.wenku8.net/novel/1/1973/69567.htm"
-html = etree.HTML(getPage(url))
+for i in href:
+    i = i+''
+    write_to_href(i)
 
-#会把每一次取出来的东西放在List里面
-title = html.xpath('//div[@id="title"]/text()')
-context = html.xpath('//div[@id="content"]/text()')
+with open('href.txt', 'r') as f:
+    while(1):
+        str = f.readline()
+        if (not str):
+            break
+        # 爬取某一页上的内容
+        url = "https://www.wenku8.net/novel/1/1973/"+str
+        # time.sleep(1)
+        html = etree.HTML(getPage(url))
+
+        #会把每一次取出来的东西放在List里面
+        title = html.xpath('//div[@id="title"]/text()')
+        context = html.xpath('//div[@id="content"]/text()')
 
 
-write_to_file(title[0]) # 因为title只有一个，而xpath提取出来会放在数组里，所以只用写第一个
+        write_to_file(title[0]) # 因为title只有一个，而xpath提取出来会放在数组里，所以只用写第一个
 
-for a in context:
-    a = a+''#change to String
-    write_to_file(a.strip()) #去除字符串前后中的一些非打印字符
+        for a in context:
+            a = a+''#change to String
+            write_to_file(a.strip()) #去除字符串前后中的一些非打印字符
